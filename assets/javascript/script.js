61dd158176c99d07247cf66e7e9d1d1c0df770dd
Melissa Wolowicz <melissawolowicz@MW-iMac.local>
2017-12-06 12:50:07 -0500

    Page is working, using Firebase. One undefined error

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,31 @@
 //IMPORTANT!
 $(document).ready(function(){
 
-	// GLOBAL VARIABLES
-	// ================
+// INITIALIZING FIREBASE
+// ================
+	var config = {
+		apiKey: "AIzaSyCfjkx7s9u5xqmTYaHgSMoJl8YL9qnFTKY",
+		authDomain: "train-scheduler-56479.firebaseapp.com",
+		databaseURL: "https://train-scheduler-56479.firebaseio.com",
+		projectId: "train-scheduler-56479",
+		storageBucket: "train-scheduler-56479.appspot.com",
+		messagingSenderId: "65819523727"
+	};
 
-	// FUNCTIONS
-	// =========
+	firebase.initializeApp(config);
 
-	// MAIN PROCESS
-	// ============
+// GLOBAL VARIABLES
+// ================
+	var userInptTrainName = "";
+	var userInptDestination = "";
+	var userInptFirstTT = "";
+	var userInptFreq = 0;
+
+// FUNCTIONS
+// =========
+
+// MAIN PROCESS
+// ============
 
 	// Capture button click
 	$("#submitBttn").on("click", function(event) {
@@ -17,16 +34,46 @@ $(document).ready(function(){
 		event.preventDefault();
 
 		//Capture the user's input for each field and store into variable.
-		var userInptTrainName = $("#trainName").val().trim();
-		var userInptDestination = $("#destination").val().trim();
-		var userInptFirstTT = $("#firstTrainTime").val().trim();
-		var userInptFreq = $("#frequency").val().trim();
+		userInptTrainName = $("#trainName").val().trim();
+		userInptDestination = $("#destination").val().trim();
+		userInptFirstTT = $("#firstTrainTime").val().trim();
+		userInptFreq = $("#frequency").val().trim();
 
-        // TESTING & DEBUGGING
+	    // TESTING & DEBUGGING
 		console.log(userInptTrainName);
 		console.log(userInptDestination);
 		console.log(userInptFirstTT);
 		console.log(userInptFreq);
+
+		//Adding information to the Firebase database
+		firebase.database().ref().push({
+			dataTrainName: userInptTrainName,
+			dataDestination: userInptDestination,
+			dataFirstTrainTime: userInptFirstTT,
+			dataFrequency: userInptFreq,
+			timeAdded: firebase.database.ServerValue.TIMESTAMP
+		});
+
+		//With a listener, capture when a new item has been added to the database.
+		firebase.database().ref().limitToLast(1).on("child_added", function(snapshot) {
+			//Printout every new Train into a new row on the table:
+				//Create a variable that HOLDS the new row
+				var newRowItem = $("<tr><td>" + snapshot.val().dataTrainName + "</td><td>" + snapshot.val().dataDestination + "</td><td>" + snapshot.val().dataFirstTrainTime + "</td><td>" + snapshot.val().dataFrequency + "</td></tr>");
+
+				//Get the table and add new row to table at the end
+				$("table tbody").append(newRowItem);
+
+		});
+
 	});
+
+
+
+
+
+
+
+
+
 
 }); //IMPORTANT!
